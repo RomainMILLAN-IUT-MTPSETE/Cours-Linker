a133ada1e67d4cc9acbf49b856393c66cdca527a
MILLAN Romain <romain.millan@wabezeter.fr>
2022-09-16 08:49:40 +0200

    Finish C

--- Semestre 3/[R3.05] Programmation Système/TD/TD n°1/shell/shell.c
+++ Semestre 3/[R3.05] Programmation Système/TD/TD n°1/shell/shell.c
@@ -1,33 +1,51 @@
-#include <sys/types.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main(void){
 
     while(1){
         char command[300];
         char *args;
-        char **cmd;
-        printf("Commande: ");
-        fgets(command, sizeof(command), stdin);  // read the command
+        char *cmd[10] = {NULL};
+        int dowait = 0;
 
+        printf("Commande: "); //Print 'Commande:'
+        fgets(command, sizeof(command), stdin);  // read the command
+        command[strcspn(command, "\n")] = 0; //remove /n at the end
         args = strtok (command, " "); //Get all arguments by the command
+
+        //Tant que args et différent de NULL
+        int i=0;
         while(args != NULL){
-            char *cmdargs;
-            printf("%s\n", &args[0]);
+            //id args is '&'
+            if(strcmp(&args[0], "&") == 0){
+                //To dowait to 1
+                dowait = 1;
+            }else {
+                //Put in cmd tab the args
+                cmd[i] = args;
+                i++;
+            }
+            //Pass to next argument
             args = strtok(NULL, " ");
         }
 
-        execv("./" + &args[0], NULL);
-        // example.out => ./example.out
-        // example.out txt.txt => ./example.out
+        //Child creation
+        pid_t pid = fork();
+        if(pid == 0){
+            //Child
+            execvp(cmd[0], cmd);
+        }
 
-        execl("ps", "ps", "-l", NULL);
-
-        
-        return 0;
+        //if do wait
+        if(dowait == 0){
+            //Wait
+            waitpid(pid, NULL, 0);
+        }
     }
 
     
