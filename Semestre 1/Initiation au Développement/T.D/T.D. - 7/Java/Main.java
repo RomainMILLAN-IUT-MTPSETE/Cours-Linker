62e3657c7fcaff05c19139f228da73168271ac89
MILLAN Romain <romain.millan@wabezeter.fr>
2022-10-20 18:10:23 +0200

    Delete All Files

--- Semestre 1/Initiation au Développement/T.D/T.D. - 7/Java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        Voiture v1 = new Voiture("PIERRE", 3);
-        Voiture v2 = new Voiture("NADAL", 4);
-        Course c = new Course(v1, v2, 100);
-
-        System.out.println(c.courseAller());
-    }
-}
