61e5758ff367101164af509b50235164a6bd11c2
MILLAN Romain <romain.millan@wabezeter.fr>
2022-10-23 10:45:04 +0200

    Modification PL:SQL to PLSQL

---
+++ Semestre 3/[CL-4] Analyse/TD/TD 3/Bis/Ex1/Java/Categorie.java
@@ -0,0 +1,7 @@
+public class Categorie {
+    private String nom;
+
+    public Categorie(String nom){
+        this.nom = nom;
+    }
+}
---
+++ Semestre 3/[CL-4] Analyse/TD/TD 3/Bis/Ex1/Java/Produit.java
@@ -0,0 +1,12 @@
+class Produit{
+    private String nom;
+    private Categorie categorie;
+
+    public Produit(String nom, Categorie categorie){
+        this.nom = nom;
+        this.categorie = categorie;
+    }
+
+    //EX1.4
+    
+}
